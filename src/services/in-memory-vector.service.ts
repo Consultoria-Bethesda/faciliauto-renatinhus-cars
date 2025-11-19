a9c6d7af5d24a3a4f081794fad8fe38fc10dd301
Rafael Novaes <rafaelnovaes22@gmail.com>
2025-11-19 16:49:56 -0300

    fix: usar OpenAI embeddings ao invés de Jina

--- src/services/in-memory-vector.service.ts
+++ src/services/in-memory-vector.service.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { generateEmbedding } from '../lib/chromadb';
+import { generateEmbedding } from '../lib/embeddings';
 
 const prisma = new PrismaClient();
 
